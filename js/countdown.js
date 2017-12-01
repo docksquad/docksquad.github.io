5216c2d8478be5c9c8075a8c3b628e08e0c55bf5
docksquad <docksquad@outlook.com>
2017-12-01 02:18:57 -0800

    update holiday

--- js/countdown.js
+++ js/countdown.js
@@ -18,16 +18,24 @@ $(document).ready(function() {
 
 	function initializeClock(id, endtime){
 	  var clock = document.getElementById(id);
-	  var timeinterval = setInterval(function(){
-	    var t = getTimeRemaining(endtime);
-	    clock.innerHTML = t.days + ':' +
-	                      t.hours + ':' +
-	                      t.minutes + ':' +	
-	                      t.seconds;
-	    if(t.total<=0){
-	      clearInterval(timeinterval);
-	    }
-	  },1000);
+	  var daysSpan = clock.querySelector('.days');
+	  var hoursSpan = clock.querySelector('.hours');
+	  var minutesSpan = clock.querySelector('.minutes');
+	  var secondsSpan = clock.querySelector('.seconds');
+
+	  function updateClock(){
+		  var t = getTimeRemaining(endtime);
+		  daysSpan.innerHTML = ('0' + t.days).slice(-2);
+		  hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+		  minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+		  secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+		  if(t.total<=0){
+		    clearInterval(timeinterval);
+		  }
+		}
+
+		updateClock(); // run function once at first to avoid delay
+		var timeinterval = setInterval(updateClock,1000);
 	}
 
 	initializeClock('clockdiv', deadline);
